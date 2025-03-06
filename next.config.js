3ecbf74aef8295257a231fbc12185c727716457d
Kleascm <kleascm@gmail.com>
2025-03-06 01:17:30 -0800

    wef

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', 
+    reactStrictMode: true,
+    experimental: {
+      appDir: true, 
+    },
+  };
+  
+  module.exports = nextConfig;
+  
